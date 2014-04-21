f0c5bfe6acc51acb844a178a978ca5778153338b
Ilya Dynin <idynin@gmail.com>
2014-04-21 16:12:23 -0400

    Fixed visibility bug

--- src/main/java/org/apache/storm/hdfs/bolt/HdfsBolt.java
+++ src/main/java/org/apache/storm/hdfs/bolt/HdfsBolt.java
@@ -38,86 +38,88 @@ import java.net.URI;
 import java.util.EnumSet;
 import java.util.Map;
 
-public class HdfsBolt extends AbstractHdfsBolt{
-    private static final Logger LOG = LoggerFactory.getLogger(HdfsBolt.class);
+public class HdfsBolt extends AbstractHdfsBolt {
+	private static final Logger LOG = LoggerFactory.getLogger(HdfsBolt.class);
 
-    private FSDataOutputStream out;
-    private RecordFormat format;
-    private long offset = 0;
+	private FSDataOutputStream out;
+	private RecordFormat format;
+	private long offset = 0;
 
-    public HdfsBolt withFsUrl(String fsUrl){
-        this.fsUrl = fsUrl;
-        return this;
-    }
+	public HdfsBolt withFsUrl(String fsUrl) {
+		this.fsUrl = fsUrl;
+		return this;
+	}
 
-    public HdfsBolt withFileNameFormat(FileNameFormat fileNameFormat){
-        this.fileNameFormat = fileNameFormat;
-        return this;
-    }
+	public HdfsBolt withFileNameFormat(FileNameFormat fileNameFormat) {
+		this.fileNameFormat = fileNameFormat;
+		return this;
+	}
 
-    public HdfsBolt withRecordFormat(RecordFormat format){
-        this.format = format;
-        return this;
-    }
+	public HdfsBolt withRecordFormat(RecordFormat format) {
+		this.format = format;
+		return this;
+	}
 
-    public HdfsBolt withSyncPolicy(SyncPolicy syncPolicy){
-        this.syncPolicy = syncPolicy;
-        return this;
-    }
+	public HdfsBolt withSyncPolicy(SyncPolicy syncPolicy) {
+		this.syncPolicy = syncPolicy;
+		return this;
+	}
 
-    public HdfsBolt withRotationPolicy(FileRotationPolicy rotationPolicy){
-        this.rotationPolicy = rotationPolicy;
-        return this;
-    }
+	public HdfsBolt withRotationPolicy(FileRotationPolicy rotationPolicy) {
+		this.rotationPolicy = rotationPolicy;
+		return this;
+	}
 
-    public HdfsBolt addRotationAction(RotationAction action){
-        this.rotationActions.add(action);
-        return this;
-    }
+	public HdfsBolt addRotationAction(RotationAction action) {
+		this.rotationActions.add(action);
+		return this;
+	}
 
-    @Override
-    public void doPrepare(Map conf, TopologyContext topologyContext, OutputCollector collector) throws IOException {
-        LOG.info("Preparing HDFS Bolt...");
-        this.fs = FileSystem.get(URI.create(this.fsUrl), hdfsConfig);
-    }
+	@Override
+	public void doPrepare(Map conf, TopologyContext topologyContext, OutputCollector collector)
+			throws IOException {
+		LOG.info("Preparing HDFS Bolt...");
+		this.fs = FileSystem.get(URI.create(this.fsUrl), hdfsConfig);
+	}
 
-    public void execute(Tuple tuple) {
-        try {
-            byte[] bytes = this.format.format(tuple);
-            out.write(bytes);
-            this.offset += bytes.length;
+	public void execute(Tuple tuple) {
+		try {
+			byte[] bytes = this.format.format(tuple);
+			out.write(bytes);
+			this.offset += bytes.length;
 
-            this.collector.ack(tuple);
+			this.collector.ack(tuple);
 
-            if(this.syncPolicy.mark(tuple, this.offset)){
-                if(this.out instanceof HdfsDataOutputStream){
-                    ((HdfsDataOutputStream)this.out).hsync(EnumSet.of(SyncFlag.UPDATE_LENGTH));
-                } else {
-                    this.out.hsync();
-                }
-                this.syncPolicy.reset();
-            }
-            if(this.rotationPolicy.mark(tuple, this.offset)){
-                rotateOutputFile();
-                this.offset = 0;
-                this.rotationPolicy.reset();
-            }
-        } catch (IOException e) {
-            LOG.warn("write/sync failed.", e);
-            this.collector.fail(tuple);
-        }
-    }
+			if (this.syncPolicy.mark(tuple, this.offset)) {
+				if (this.out instanceof HdfsDataOutputStream) {
+					((HdfsDataOutputStream) this.out).hsync(EnumSet.of(SyncFlag.UPDATE_LENGTH));
+				} else {
+					this.out.hsync();
+				}
+				this.syncPolicy.reset();
+			}
+			if (this.rotationPolicy.mark(tuple, this.offset)) {
+				rotateOutputFile();
+				this.offset = 0;
+				this.rotationPolicy.reset();
+			}
+		} catch (IOException e) {
+			LOG.warn("write/sync failed.", e);
+			this.collector.fail(tuple);
+		}
+	}
 
-    @Override
-    void closeOutputFile() throws IOException {
-        this.out.hsync();
-        this.out.close();
-    }
+	@Override
+	public void closeOutputFile() throws IOException {
+		this.out.hsync();
+		this.out.close();
+	}
 
-    @Override
-    Path createOutputFile() throws IOException {
-        Path path = new Path(this.fileNameFormat.getPath(), this.fileNameFormat.getName(this.rotation, System.currentTimeMillis()));
-        this.out = this.fs.create(path);
-        return path;
-    }
+	@Override
+	public Path createOutputFile() throws IOException {
+		Path path = new Path(this.fileNameFormat.getPath(), this.fileNameFormat.getName(
+				this.rotation, System.currentTimeMillis()));
+		this.out = this.fs.create(path);
+		return path;
+	}
 }
